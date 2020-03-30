fc22491151593b7fa108e603e1219cd96ffde873
Lukas <lukas.schneider70@yahoo.de>
2020-03-30 16:12:25 +0200

    added delete and archive functions to praxissemester

--- src/router/index.js
+++ src/router/index.js
@@ -47,17 +47,7 @@ const routes = [
     path: '/publikationen',
     name: 'Publikationen',
     component: () => import('../views/Publikationen.vue')
-  }  
-  
-  //,
-  //{
-  //  path: '/about',
-  //  name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-  //  component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  //}
+  }
 ]
 
 const router = new VueRouter({
--- src/views/Praxissemester.vue
+++ src/views/Praxissemester.vue
@@ -1,38 +1,49 @@
 <template>
-<div>
-    <WingHeader title="Praxissemester"/>
-    <ul class="list-group">
-        <li v-for="data in praxissemester" v-bind:key="data.author" class="list-group-item d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-            {{data.author}}
-            <div>
-                <button class="btn btn-outline-primary mx-1">
-                    <font-awesome-icon icon="edit" />
-                </button>
-                <button class="btn btn-outline-warning mx-1">
-                    <font-awesome-icon icon="archive" />
-                </button>
-                <button class="btn btn-outline-danger mx-1">
-                    <font-awesome-icon icon="trash" />
-                </button>
-            </div>
-        </li>
-    </ul>
-</div>
+    <div>
+        <WingHeader title="Praxissemester"/>
+        <ul class="list-group">
+            <li v-for="data in praxissemester" v-bind:key="data.author" class="list-group-item d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+                {{data.author}}
+                <div>
+                    <button class="btn btn-outline-primary mx-1">
+                        <font-awesome-icon icon="edit" />
+                    </button>
+                    <button class="btn btn-outline-warning mx-1" @click="archiveItem(data)" >
+                        <font-awesome-icon icon="archive" />
+                    </button>
+                    <button class="btn btn-outline-danger mx-1" @click="deleteItem(data)" >
+                        <font-awesome-icon icon="trash" />
+                    </button>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 import WingHeader from '../components/WingHeader'
 import json from '../json/praxissemester.json'
+import archive_json from '../json/archive/praxissemester.json'
+
 export default {
   name: 'Praxissemester',
   components: {
     WingHeader
   },
   data(){
-          console.log(json.berichte);
-      return{
-          praxissemester: json.berichte
-      }
+    return{
+        praxissemester: json.berichte,
+        archive: archive_json.berichte
+    }
+  },
+  methods: {
+    deleteItem: function(item) {
+        this.praxissemester.splice(this.praxissemester.indexOf(item), 1);
+    },
+    archiveItem: function(item) {
+        this.praxissemester.splice(this.praxissemester.indexOf(item), 1);
+        this.archive.push(item);
+    }
   }
 }
 </script>
