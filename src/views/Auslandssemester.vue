08afb83b6e37d420060f5e3f67d278dcb8696292
Lukas <lukas.schneider70@yahoo.de>
2020-03-30 16:26:39 +0200

    added delete and archive functions to auslandssemester

--- src/views/Auslandssemester.vue
+++ src/views/Auslandssemester.vue
@@ -2,7 +2,7 @@
     <div>
         <WingHeader title="Auslandssemester"/>
         <ul class="list-group">
-            <li v-for="data in praxissemester" v-bind:key="data.bericht_author" class="list-group-item d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+            <li v-for="data in auslandssemester" v-bind:key="data.bericht_author" class="list-group-item d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
                 <div>
                     <div>
                         {{data.bericht_title}}
@@ -17,10 +17,10 @@
                     <button class="btn btn-outline-primary mx-1">
                         <font-awesome-icon icon="edit" />
                     </button>
-                    <button class="btn btn-outline-warning mx-1">
+                    <button class="btn btn-outline-warning mx-1" @click="archiveItem(data)" >
                         <font-awesome-icon icon="archive" />
                     </button>
-                    <button class="btn btn-outline-danger mx-1">
+                    <button class="btn btn-outline-danger mx-1" @click="deleteItem(data)" >
                         <font-awesome-icon icon="trash" />
                     </button>
                 </div>
@@ -32,16 +32,26 @@
 <script>
 import WingHeader from '../components/WingHeader'
 import json from '../json/auslandssemester.json'
+import archive_json from '../json/archive/auslandssemester.json'
 export default {
   name: 'Auslandssemester',
   components: {
     WingHeader
   },
   data(){
-          console.log(json.berichte);
-      return{
-          praxissemester: json.berichte
-      }
+    return{
+        auslandssemester: json.berichte,
+        archive: archive_json.berichte,
+        }
+  },
+  methods: {
+    deleteItem: function(item) {
+        this.auslandssemester.splice(this.auslandssemester.indexOf(item), 1);
+    },
+    archiveItem: function(item) {
+        this.auslandssemester.splice(this.auslandssemester.indexOf(item), 1);
+        this.archive.push(item);
+    }
   }
 }
 </script>
