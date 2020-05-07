9cd0d80c1a2f76f239c2d1792cb11e363aa14478
Lukas <lukas.schneider70@yahoo.de>
2020-05-07 14:54:40 +0200

    cors hinzugefügt

--- server.js
+++ server.js
@@ -2,22 +2,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 //HTTP request
 app.use(morgan('tiny'));
-
-
-
+app.use(cors());
 
 
 //URL= http//localhost:2019/MyWebApp/<Dateipfad> unterhalb des Wurzelverzeichnisses <Pfad der ajaxserver.js Dateii/public
 //app.use('/WingSever',express.static(__dirname +'/public'));
-
-
- 
 
 /*app.get('/api', (req, res) =>{
     const data = {
