c2e97cc5266ce2d81edbdcb4f3c44d87fb72943d
Lukas <lukas.schneider70@yahoo.de>
2020-05-29 13:43:50 +0200

    TimyMCE eingefügt

---
+++ client/src/components/HtmlEditor.vue
@@ -0,0 +1,25 @@
+<template>
+    <Editor
+          :init="{
+            height: 500,
+            menubar : false, 
+            plugins: [
+                'advlist autolink lists link charmap preview anchor visualblocks code fullscreen table paste'
+            ],
+            toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link'
+          }"
+          v-model="value"
+        />
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue'
+
+export default {
+    props: ['value'],
+    name: 'HtmlEditor',
+    components: {
+        Editor
+    }
+}
+</script>
