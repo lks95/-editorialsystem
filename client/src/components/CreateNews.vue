17570427841e885bd93a132095c041e8d241dcef
lk104 <lk104@hdm-stuttgart.de>
2020-06-16 19:53:16 +0200

    api endpoint ausgelagert // view + component von news
    editnews, createnews componenten angelegt und news view. validations sind noch nicht implementiert. img auswahl muss noch überarbeitet werden

--- client/src/components/CreateNews.vue
+++ client/src/components/CreateNews.vue
@@ -1,13 +1,93 @@
 <template>
-    
+    <div>
+        <form id="addTermineForm" @submit.prevent="submit" class="pb-2 mb-3 mr-3 border-bottom">
+            <div class="form-group" :class="{'form-group--error': $v.title.$error}">
+                <label for="titleInput">Titel:</label>
+                <input type="text" class="form-control" id="titleInput" v-model.trim="title" @input="updateTitel($event.target.value)">
+            </div>
+
+            <div class="form-group" :class="{'form-group--error': $v.link.$error}">
+                <label for="linkInput">Link:</label>
+                <input type="text" class="form-control" id="linkInput" v-model.trim="link" @input="updateLink($event.target.value)">
+            </div>
+
+            <div class="form-group" :class="{'form-group--error': $v.linkText.$error}">
+                <label for="descriptionInput">Linktext:</label>
+                <input type="text" class="form-control" id="descriptionInput" v-model.trim="linkText" @input="updateLinktext($event.target.value)">
+            </div>
+
+            <div class="form-group" :class="{'form-group--error': $v.place.$error}">
+                <label for="placeInput">Bild:</label>
+                <input type="image" class="form-control" id="placeInput" v-model.trim="img" @input="updateImg($event.target.value)">
+            </div>
+
+            <div class="form-group" :class="{'form-group--error': $v.contact.$error}">
+                <label for="textInput">Text:</label>
+                <input type="text" class="form-control" id="textInput" v-model.trim="text" @input="updateText($event.target.value)">
+            </div>
+
+            <div class="d-flex flex-row-reverse">
+                <button type="submit" class="btn btn-primary" :disabled="submitStatus === 'PENDING'">Speichern</button>
+                <button class="btn btn-outline-primary mx-2" @click="$emit('cancel')">Abbrechen</button>
+                <p class="typo__p" v-if="submitStatus === 'ERROR'">Fill Form correctly</p>
+                <p class="typo__p" v-if="submitStatus === 'PENDING'">Sendet...</p>
+            </div>
+
+        </form>
+    </div>
 </template>
+
 
 <script>
     export default {
-        name: "CreateNews.vue"
+        name: "CreateNews",
+        data(){
+            return{
+                title: '',
+                link: '',
+                linkText: '',
+                img: '',
+                text: '',
+                submitStatus: null,
+            };
+        },
+        methods: {
+            submit: function() {
+                this.$v.$touch()
+                    let formData = {
+                        title: this.title,
+                        link: this.link,
+                        linkText: this.linkText,
+                        img: this.img,
+                        text: this.text,
+                    };
+                    this.$emit("save", formData);
+                    this.submitStatus = 'PENDING'
+                    setTimeout(() => {
+                        this.submitStatus = 'OK'
+                    }, 500)
+                },
+
+            updateTitel(value){
+                this.title = value;
+                this.$v.title.$touch();
+            },
+            updateLinktext(value){
+                this.linkText = value;
+                this.$v.linkText.$touch();
+            },
+            updateLink(value){
+                this.link = value;
+                this.$v.link.$touch();
+            },
+            updateImg(value){
+                this.img = value;
+                this.$v.img.$touch();
+            },
+            updateText(value){
+                this.text = value;
+                this.$v.text.$touch();
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
--- client/src/views/News.vue
+++ client/src/views/News.vue
@@ -1,9 +1,48 @@
 <template>
-    <WingHeader title="News" @selectArchive="selectArchive" @addNew="addItem"/>
+    <div>
+        <WingHeader title="News" @selectArchive="selectArchive" @addNew="addItem" />
+        <CreateNews v-if="showForm" @save="saveNew" @cancel="cancelNew"/>
+        <LoadingSpinner v-if="!dataLoaded" />
+        <draggable v-else-if="!displayArchive" v-model="termine" group="termine" @start="drag=true" @end="drag=false" handle=".handle">
+            <div v-for="data in termine" v-bind:key="data.tid" class="list-group-item">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center handle">
+                    <div>
+                        <font-awesome-icon icon="grip-vertical" class="mr-3 text-muted"/>
+                        {{data.news}}
+                    </div>
+                    <div class="d-flex">
+                        <div>
+                            <button v-b-toggle="'collapse-' + data.tid" variant="primary" class="btn btn-outline-primary mx-1">
+                                <font-awesome-icon icon="edit" />
+                            </button>
+
+                            <button class="btn btn-outline-warning mx-1" @click="confirmArchive(data)" >
+                                <font-awesome-icon icon="archive" />
+                            </button>
+
+                            <button class="btn btn-outline-danger mx-1" @click="confirmDelete(data)" >
+                                <font-awesome-icon icon="trash" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <b-collapse :id="'collapse-' + data.nId" class="border-top mt-3">
+                    <EditNews :selectedItem="data" :selectedIndex="data.nId"  @save="updateItem" />
+                </b-collapse>
+
+            </div>
+        </draggable>
+    </div>
 </template>
 
 <script>
 import WingHeader from '../components/WingHeader'
+import draggable from 'vuedraggable'
+import axios from "axios";
+import CreateNews from "../components/CreateNews";
+import LoadingSpinner from "../components/LoadingSpinner";
+import EditNews from "../components/EditNews";
+
 export default {
   name: 'News',
   components: {
@@ -33,9 +72,128 @@ export default {
       }
   },
   methods: {
-      saveToBackend: function(){
+      addItem: function(){
+          this.showForm = true;
+      },
+      saveNew: function(newItem){
+          newItem.nId = this.nIndex;
+          this.nIndex++;
+          this.news.unshift(newItem);
+          this.displayArchive = false;
+          this.showForm = false;
+          this.selectedItem = {};
+          this.saveToBackend();
+      },
+      cancelNew: function(){
+          this.showForm = false;
+      },
+      updateItem: function(item){
+          let foundIndex = this.news.findIndex(x => x.nId === item.nId);
+          this.news[foundIndex].title = item.title;
+          this.news[foundIndex].link = item.link;
+          this.news[foundIndex].img = item.img;
+          this.text[foundIndex].img = item.text;
+          this.saveToBackend();
+      },
+      selectArchive: function(archiveSelected){
+          this.displayArchive = archiveSelected;
+      },
 
-      }
+      saveToBackend: function(){
+          axios.post('http://localhost:5000/api/news', {"news": this.news})
+              .then((res)=>{
+                  this.news = res.data.news;
+              })
+      },
+      saveArchiveToBackend: function(){
+          axios.post('http://localhost:5000/api/news/archive', {"news": this.news})
+              .then((res)=>{
+                  this.news = res.data.news;
+              })
+      },
+      confirmDelete: function(item){
+          this.$bvModal.msgBoxConfirm('Ausgewähltes Element unwiderruflich löschen?', {
+              title: 'Löschen bestätigen',
+              okVariant: 'danger',
+              cancelTitle: 'Abbrechen',
+              hideHeaderClose: true,
+              centered: true
+          })
+              .then(value => {
+                  if(value){
+                      if(this.displayArchive){
+                          this.archive.splice(this.archive.indexOf(item), 1);
+                          this.saveArchiveToBackend();
+                      }else{
+                          this.news.splice(this.news.indexOf(item), 1);
+                          this.saveToBackend();
+                      }
+                  }
+              })
+              .catch(err => {
+                  console.log(err);
+              })
+      },
+      restoreFromArchive: function(item){
+          this.$bvModal.msgBoxConfirm('Ausgewähltes Element wiederherstellen?', {
+              title: 'Wiederherstellung bestätigen',
+              okVariant: 'warning',
+              cancelTitle: 'Abbrechen',
+              hideHeaderClose: true,
+              centered: true
+          })
+              .then(value => {
+                  if(value){
+                      this.archive.splice(this.archive.indexOf(item), 1);
+                      this.news.unshift(item);
+                      this.saveToBackend();
+                      this.saveArchiveToBackend();
+                  }
+              })
+              .catch(err => {
+                  console.log(err);
+              })
+      },
+      confirmArchive: function(item) {
+          this.$bvModal.msgBoxConfirm('Ausgewähltes Element archivieren?', {
+              title: 'Archivieren bestätigen',
+              okVariant: 'warning',
+              cancelTitle: 'Abbrechen',
+              hideHeaderClose: true,
+              centered: true
+          })
+              .then(value => {
+                  if(value){
+                      this.news.splice(this.news.indexOf(item), 1);
+                      this.archive.unshift(item);
+                      this.saveToBackend();
+                      this.saveArchiveToBackend();
+                  }
+              })
+              .catch(err => {
+                  console.log(err);
+              })
+      },
+  },
+  mounted() {
+        axios.get("http://localhost:5000/api/news").then(
+            response =>
+                (this.news = response.data.news.map(item => {
+                        item.nId = this.nIndex;
+                        this.nIndex++;
+                        return item;
+                    }),
+                        this.dataLoaded = true
+                )
+        );
+        axios.get("http://localhost:5000/api/news/archive").then(
+            response =>
+                (this.archive = response.data.news.map(item => {
+                    item.nId = this.nIndex;
+                    this.nIndex++;
+                    return item;
+                }))
+        );
   }
 }
 </script>
