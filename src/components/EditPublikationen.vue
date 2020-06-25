79317da1ff8da5f7c035ff0fd3597e699d486e64
lk104 <lk104@hdm-stuttgart.de>
2020-06-25 18:26:15 +0200

    Publikationen for Create / Edit / View
    Validation noch nicht implementiert

---
+++ src/components/EditPublikationen.vue
@@ -0,0 +1,144 @@
+<template>
+    <div>
+        <form id="addForm" @submit.prevent="submit"  class="pb-2 mb-3 mr-3">
+
+            <div class="form-group">
+                <label for="titleInput">Titel:</label>
+                <input type="text" class="form-control"  id="titleInput" v-model.trim="publikation_title"  @input="updateTitle($event.target.value)">
+            </div>
+
+            <div class="form-group">
+                <label for="subtitleInput">Untertitel:</label>
+                <input type="text" class="form-control" id="subtitleInput" v-model.trim="publikation_subtitle" @input="updateSubtitle($event.target.value)">
+            </div>
+
+            <div class="form-group">
+                <label for="textInput">Text:</label>
+                <textarea class="form-control" id="textInput" rows="3" v-model.trim="publikation_title" @input="updateText($event.target.value)"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="authorInput">Author:</label>
+                <textarea class="form-control" id="authorInput" rows="3" v-model.trim="publikation_author" @input="updateAuthor($event.target.value)"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="betreuerInput">Betreuer:</label>
+                <textarea class="form-control" id="betreuerInput" rows="3" v-model.trim="publikation_betreuer" @input="updateBetreuer($event.target.value)"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="imgInput">Bild:</label>
+                <textarea class="form-control" id="imgInput" rows="3" v-model.trim="img" @input="updateImg($event.target.value)"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="pdfInput">Pdf:</label>
+                <textarea class="form-control" id="pdfInput" rows="3" v-model.trim="pdf" @input="updatePdf($event.target.value)"></textarea>
+            </div>
+
+
+
+            <div class="d-flex flex-row-reverse">
+                <button type="submit" class="btn btn-primary" :disabled="submitStatus === 'PENDING'" >Speichern</button>
+                <b-button class="mx-2" v-b-toggle="'collapse-' + selectedIndex" @click="resetForm">Abbrechen</b-button>
+                <p class="typo__p" v-if="submitStatus === 'ERROR'">Please fill the form correctly.</p>
+                <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EditPublikationen",
+        props: ["selectedItem", "selectedIndex"],
+        data() {
+            return{
+                publikation_title: '',
+                publikation_subtitle: '',
+                publikation_text: '',
+                publikation_author: '',
+                publikation_betreuer: '',
+                img: '',
+                pdf: '',
+                submitStatus: null,
+            };
+        },
+        methods: {
+            submit: function() {
+                this.$v.$touch()
+
+                    let formData = {
+                        publikation_title: this.publikation_title || this.selectedItem.publikation_title,
+                        publikation_subtitle: this.publikation_subtitle || this.selectedItem.publikation_subtitle,
+                        publikation_text: this.publikation_text || this.selectedItem.publikation_text,
+                        publikation_author: this.publikation_author || this.selectedItem.publikation_author,
+                        publikation_betreuer: this.publikation_betreuer || this.selectedItem.publikation_betreuer,
+                        img: this.img || this.selectedItem.img,
+                        pdf: this.pdf || this.selectedItem.pdf,
+                        pId: this.selectedIndex
+                    }
+                    this.$emit("save", formData);
+                    this.submitStatus = 'PENDING'
+                    setTimeout(() => {
+                        this.submitStatus = 'OK'
+                    }, 500)
+
+                this.$root.$emit('bv::toggle::collapse', 'collapse-' + this.selectedIndex)
+            },
+
+            updateTitle(value){
+                this.publikation_title = value;
+                this.$v.publikation_title.$touch();
+            },
+            updateSubtitle(value){
+                this.publikation_subtitle = value;
+                this.$v.publikation_subtitle.$touch();
+            },
+            updateText(value){
+                this.publikation_text = value;
+                this.$v.publikation_text.$touch();
+            },
+            updateAuthor(value){
+                this.publikation_author = value;
+                this.$v.publikation_author.$touch();
+            },
+            updateBetreuer(value){
+                this.publikation_betreuer = value;
+                this.$v.publikation_betreuer.$touch();
+            },
+            updateImg(value){
+                this.img = value;
+                this.$v.img.$touch();
+            },
+            updatePdf(value){
+                this.pdf = value;
+                this.$v.pdf.$touch();
+            },
+
+            resetForm(){
+                this.publikation_title= this.selectedItem.publikation_title;
+                this.publikation_subtitle= this.selectedItem.publikation_subtitle;
+                this.publikation_text= this.selectedItem.publikation_text;
+                this.publikation_author= this.selectedItem.publikation_author;
+                this.publikation_betreuer= this.selectedItem.publikation_betreuer;
+                this.img= this.selectedItem.img;
+                this.pdf= this.selectedItem.pdf;
+            }
+        },
+        mounted(){
+            this.publikation_title= this.selectedItem.publikation_title;
+            this.publikation_subtitle= this.selectedItem.publikation_subtitle;
+            this.publikation_text= this.selectedItem.publikation_text;
+            this.publikation_author= this.selectedItem.publikation_author;
+            this.publikation_betreuer= this.selectedItem.publikation_betreuer;
+            this.img= this.selectedItem.img;
+            this.pdf= this.selectedItem.pdf;
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Publikationen.vue
+++ src/views/Publikationen.vue
@@ -1,13 +1,217 @@
 <template>
-    <WingHeader title="Publikationen"/>
+    <div>
+        <WingHeader title="Publikationen" @selectArchive="selectArchive" @addNew="addItem" />
+        <CreatePublikationen v-if="showForm" @save="saveNew" @cancel="cancelNew" />
+        <LoadingSpinner v-if="!dataLoaded" />
+        <draggable v-else-if="!displayArchive" v-model="publikationen" group="publikationen" @start="drag=true" @end="drag=false;" handle=".handle" @change="saveToBackend()">
+            <div v-for="data in publikationen" v-bind:key="data.pId" class="list-group-item">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center handle">
+                    <div>
+                        <font-awesome-icon icon="grip-vertical" class="mr-3 text-muted"/>
+                        {{data.publikation_title}}
+                    </div>
+                    <div class="d-flex">
+                        <div>
+                            <button v-b-toggle="'collapse-' + data.pId" variant="primary" class="btn btn-outline-primary mx-1">
+                                <font-awesome-icon icon="edit" />
+                            </button>
+                            <button class="btn btn-outline-warning mx-1" @click="confirmArchive(data)" >
+                                <font-awesome-icon icon="archive" />
+                            </button>
+                            <button class="btn btn-outline-danger mx-1" @click="confirmDelete(data)" >
+                                <font-awesome-icon icon="trash" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <b-collapse :id="'collapse-' + data.pId" class="border-top mt-3">
+                    <EditPublikationen :selectedItem="data" :selectedIndex="data.pId"  @save="updateItem" />
+                </b-collapse>
+            </div>
+        </draggable>
+        <draggable v-else v-model="archive" group="publikationen-archive" @start="drag=true" @end="drag=false" @change="saveArchiveToBackend()">
+            <div v-for="data in archive" v-bind:key="data.pId" class="list-group-item d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center drag-drop">
+                <div>
+                    <font-awesome-icon icon="grip-vertical" class="mr-3 text-muted"/>
+                    {{data.publikation_title}}
+                </div>
+                <div class="d-flex">
+                    <div>
+                        <button class="btn btn-outline-warning mx-1" @click="restoreFromArchive(data)" >
+                            <font-awesome-icon icon="undo" />
+                        </button>
+                        <button class="btn btn-outline-danger mx-1" @click="confirmDelete(data)" >
+                            <font-awesome-icon icon="trash" />
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </draggable>
+    </div>
 </template>
 
 <script>
 import WingHeader from '../components/WingHeader'
+import axios from "axios";
+import EditPublikationen from "../components/EditPublikationen";
+import CreatePublikationen from "../components/CreatePublikationen";
+import LoadingSpinner from "../components/LoadingSpinner";
+import draggable from 'vuedraggable'
+
 export default {
   name: 'Publikationen',
   components: {
-    WingHeader
-  }
+    WingHeader,
+    CreatePublikationen,
+    EditPublikationen,
+    LoadingSpinner,
+    draggable
+  },
+  data(){
+      return{
+          publikationen: [],
+          archive: [],
+          displayArchive: false,
+          showForm: false,
+          selectedItem: {},
+          pIndex: 0,
+          dataLoaded: false
+      }
+  },
+    computed: {
+        dataToShow: function () {
+            if(this.displayArchive === true){
+                return this.archive;
+            }
+            return this.publikationen;
+        }
+    },
+    methods: {
+        saveToBackend: function(){
+            axios.post('http://localhost:5000/api/publikationen', {"publikationen": this.publikationen})
+                .then((res)=>{
+                    this.publikationen = res.data.publikationen;
+                })
+        },
+        saveArchiveToBackend: function(){
+            axios.post('http://localhost:5000/api/publikationen/archive', {"publikationen": this.archive})
+                .then((res)=>{
+                    this.archive = res.data.publikationen;
+                })
+        },
+        confirmArchive: function(item) {
+            this.$bvModal.msgBoxConfirm('Ausgewähltes Element archivieren?', {
+                title: 'Archivieren bestätigen',
+                okVariant: 'warning',
+                cancelTitle: 'Abbrechen',
+                hideHeaderClose: true,
+                centered: true
+            })
+                .then(value => {
+                    if(value){
+                        this.publikationen.splice(this.publikationen.indexOf(item), 1);
+                        this.archive.unshift(item);
+                        this.saveToBackend();
+                        this.saveArchiveToBackend();
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        },
+        restoreFromArchive: function(item){
+            this.$bvModal.msgBoxConfirm('Ausgewähltes Element wiederherstellen?', {
+                title: 'Wiederherstellung bestätigen',
+                okVariant: 'warning',
+                cancelTitle: 'Abbrechen',
+                hideHeaderClose: true,
+                centered: true
+            })
+                .then(value => {
+                    if(value){
+                        this.archive.splice(this.archive.indexOf(item), 1);
+                        this.publikationen.unshift(item);
+                        this.saveToBackend();
+                        this.saveArchiveToBackend();
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        },
+        confirmDelete: function(item){
+            this.$bvModal.msgBoxConfirm('Ausgewähltes Element unwiderruflich löschen?', {
+                title: 'Löschen bestätigen',
+                okVariant: 'danger',
+                cancelTitle: 'Abbrechen',
+                hideHeaderClose: true,
+                centered: true
+            })
+                .then(value => {
+                    if(value){
+                        if(this.displayArchive){
+                            this.archive.splice(this.archive.indexOf(item), 1);
+                            this.saveArchiveToBackend();
+                        }else{
+                            this.publikationen.splice(this.publikationen.indexOf(item), 1);
+                            this.saveToBackend();
+                        }
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        },
+        selectArchive: function(archiveSelected){
+            this.displayArchive = archiveSelected;
+        },
+        addItem: function(){
+            this.showForm = true;
+        },
+        saveNew: function(newItem){
+            newItem.pId = this.pIndex;
+            this.psIndex++;
+            this.publikationen.unshift(newItem);
+            this.displayArchive = false;
+            this.showForm = false;
+            this.selectedItem = {};
+            this.saveToBackend();
+        },
+        cancelNew: function(){
+            this.showForm = false;
+        },
+        updateItem: function(item){
+            let foundIndex = this.publikationen.findIndex(x => x.pId === item.pId);
+            this.publikationen[foundIndex].publikation_title = item.publikation_title;
+            this.publikationen[foundIndex].publikation_subtitle = item.publikation_subtitle;
+            this.publikationen[foundIndex].publikation_text = item.publikation_text;
+            this.publikationen[foundIndex].publikation_author = item.publikation_author;
+            this.publikationen[foundIndex].publikation_betreuer = item.publikation_betreuer;
+            this.publikationen[foundIndex].img = item.img;
+            this.publikationen[foundIndex].pdf = item.pdf;
+            this.saveToBackend();
+        }
+    },
+    mounted() {
+        axios.get("http://localhost:5000/api/publikationen").then(
+            response =>
+                (this.publikationen = response.data.publikationen.map(item => {
+                        item.pId = this.pIndex;
+                        this.pIndex++;
+                        return item;
+                    }),
+                        this.dataLoaded = true
+                )
+        );
+        axios.get("http://localhost:5000/api/publikationen/archive").then(
+            response =>
+                (this.archive = response.data.publikationen.map(item => {
+                    item.pId = this.pIndex;
+                    this.pIndex++;
+                    return item;
+                }))
+        );
+    }
 }
 </script>
