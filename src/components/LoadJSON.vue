05ce0fcc073bfc2bad1d20b85150c9b8bccfe822
Lukas <lukas.schneider70@yahoo.de>
2020-07-22 23:43:09 +0200

    LoadJSON übersetzt

--- src/components/LoadJSON.vue
+++ src/components/LoadJSON.vue
@@ -1,21 +1,17 @@
 <template>
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mr-3 border-bottom">
-      <h1 class="h2">{{file}}</h1>
-      <h2>Upload a File</h2>
-            <div class="d-flex">
-                <div class="btn-toolbar mb-2 mb-md-0">
-                  
-                    <div class="btn-group mr-2">
-                        <div id="app"  v-cloak>
-                            <input type="file"  class="btn btn-outline-primary mx-2" ref="myFile" />
-                            <input type="submit" class="btn btn-primary" value="Upload File" @click="Upload()" />
-                        </div>  
-                        <b-button class="btn btn-outline-danger mx-1" @click="$emit('Download')" title="Load file">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mr-3">
+      <h2>Datei hochladen</h2>
+        <div class="d-flex">
+            <div class="btn-toolbar pl-3 mb-2 mb-md-0">
+                <div class="row mr-2">
+                    <input type="file"  class="btn btn-outline-primary mx-2" ref="myFile" />
+                    <input type="submit" class="btn btn-primary" value="Datei hochladen" @click="Upload()" />
+                    <b-button class="mx-1" @click="$emit('Download')" title="Datei herunterladen">
                         <b-icon icon="download" aria-hidden="true"></b-icon>
-                        </b-button>
-                    </div>
+                    </b-button>
                 </div>
             </div>
+        </div>
     </div>
 </template>
 
